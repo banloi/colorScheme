8b4d1fee6cccbbcc09666b5771003fcbcc30a3ac
Banloi.com <clear1900@protonmail.com>
2020-06-06 14:35:16 +0800

    main page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from 'react'
+import { ColorBlock, Swatche } from './components'
+import './style/container.scss'
+import data from './assets/colors.json'
+function App () {
+  const [backgroundColor, setbackgroundColor] = useState('#989898')
+  function handleSelect (color) {
+    setbackgroundColor(color)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='warapper' style={{ backgroundColor: backgroundColor }}>
+      <div className='container'>
+        <div className='list'>
+          {data.map((list) => {
+            return <ColorBlock onSelect={handleSelect} key={list.hex + list.pinyin} info={list} />
+          })}
+        </div>
+        <Swatche
+          color={backgroundColor}
+        />
+      </div>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
