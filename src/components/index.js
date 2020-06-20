5504df2e13c795443a9a1eb4f11f48f71b8fb1fc
Banloi.com <clear1900@protonmail.com>
2020-06-20 21:16:18 +0800

    feat: Add CurrentColor Gradient component collection

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import ColorBlock from './ColorBlock'
+import Gradient from './Gradient'
+import CurrentColor from './CurrentColor'
 import Swatche from './Swatche'
 
-export { ColorBlock, Swatche }
+export { ColorBlock, Gradient, CurrentColor, Swatche }
