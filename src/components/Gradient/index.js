d3d84d6e34297bd5e53cab4570d9376a9bf584d8
Banloi.com <clear1900@protonmail.com>
2020-06-20 15:48:20 +0800

    refactor: Change folder nameSwatch to Gradient

---
+++ src/components/Gradient/index.js
@@ -0,0 +1,39 @@
+import React, { useRef, useEffect } from 'react'
+import { swatcheGenerator, shadeGenerator, setFontColor } from '../../utils'
+import './index.scss'
+
+function Gradient (props) {
+  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const lists = shadeGenerator(props.color)
+  return (
+    <div className='swatche'>
+      {
+        lists.map((color, index) => {
+          return (
+            <div
+              className='swatche-item'
+              key={color + index}
+              style={{ backgroundColor: color, color: setFontColor(color) }}
+            >
+              <span>{color}</span>
+            </div>
+          )
+        })
+      }
+      {/* {list.map(index => {
+        const color = swatcheGenerator(props.color, index)
+        return (
+          <div
+            className='swatche-item'
+            key={index + color}
+            style={{ backgroundColor: color.color, color: color.fontColor }}
+          >
+            <span>{color.color}</span>
+          </div>
+        )
+      })} */}
+    </div>
+  )
+}
+
+export default Gradient
