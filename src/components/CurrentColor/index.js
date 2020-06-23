f2f261d8e57f0df70eefefd00812270ac8337c12
Banloi.com <clear1900@protonmail.com>
2020-06-23 18:39:53 +0800

    feat: add copy hex  to clipboard and tips

--- src/components/CurrentColor/index.js
+++ src/components/CurrentColor/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { setForegroundColor } from '../../utils'
 import './index.scss'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import copyDark from '../../assets/icons/copy-dark.png'
+import copyLight from '../../assets/icons/copy-light.png'
 
+// const dark = '../../assets/icons/copy-dark.png'
 function CurrentColor (props) {
   const { info, fontColor } = props
+  let copyIcon
+  if (setForegroundColor(info.hex)) {
+    copyIcon = copyDark
+  } else {
+    copyIcon = copyLight
+  }
   return (
     <div className='current-color' style={{ color: fontColor }}>
       <title>
@@ -21,8 +32,16 @@ function CurrentColor (props) {
       >
         锁定白色背景
       </button>
-    </div>
+      <div className='copy-box'>
+        <CopyToClipboard text={info.hex}>
+          <>
+            <div className='copy' style={{ backgroundImage: `url(${copyIcon})` }} />
+            <span className='tips'>点击复制 Hex</span>
+          </>
+        </CopyToClipboard>
+      </div>
 
+    </div>
   )
 }
 
